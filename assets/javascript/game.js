ca3fbce850760e126edb123bbc2c395a474ba285
ChristinaLavingia <christinalavingia@gmail.com>
2018-12-05 19:15:03 -0800

    Commiting final code with working scores.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,47 +13,48 @@ $("#startQuiz").on("click", function(event) {
 var questions = [{
         question: "Which quarterback leads the league in passing yards?",
         options: ["Patrick Mahomes", "Jared Goff", "Tom Brady", "Matt Ryan"],
-        answer: 3
+        answer: "Matt Ryan"
     }, {
         question: "Which team did Amari Cooper play for at the beginning of the season and who does he play for now?",
         options: ["Raiders, Cowboys", "49ers, Texans", "Eagles, Cowboys", "Dolphins, Rams"],
-        answer: 0
+        answer: "Raider, Cowboys"
     }, {
         question: "Name the running back who is leading the league in rushing yards.",
         options: ["Ezekiel Elliott", "Melvin Gordon", "Saquon Barkley", "Todd Gurley"],
-        answer: 0
+        answer: "Ezekiel Elliott"
     }, {
         question: "Which quarterback suffered a potentially career-ending injury in week 11?",
         options: ["Jimmy Garoppolo", "Ryan Fitzpatrick", "Nick Foles", "Alex Smith"],
-        answer: 3
+        answer: "Alex Smith"
     }, {
         question: "Which defense/special teams has the fewest sacks this season?",
         options: ["49ers", "Raiders", "Jaguars", "Giants"],
-        answer: 1
+        answer: "Raiders"
     }, {
         question: "Name the wide receiver who is leading the league in receiving yards.",
         options: ["Adam Thielen", "Tyreek Hill", "Julio Jones", "Odell Beckham"],
-        answer: 2
+        answer: "Julio Jones"
     },  {
         question: "Name the team that released Kareem Hunt after TMZ released an incriminating video.",
         options: ["Chiefs", "Rams", "Browns", "Raiders"],
-        answer: 0
+        answer: "Chiefs"
     }, {
         question: "Which quarterback has the most touchdowns this season?",
         options: ["Drew Brees", "Jared Goff", "Patrick Mahomes", "Ben Roethlisberger"],
-        answer: 2
+        answer: "Patrick Mahomes"
     }, {
         question: "Name the 2018 defense/special teams with the most interceptions.",
         options: ["Browns", "Bears", "Chiefs", "Dolphins"],
-        answer: 1
+        answer: "Bears"
     }, {
         question: "Which tight end has the most targets this season?",
         options: ["Travis Kelce", "George Kittle", "Jared Cook", "Zach Ertz"],
-        answer: 3
+        answer: "Zach Ertz"
     }];
 
 //Stores quiz function
 var quiz = {
+    total: 10,
     correct: 0,
     incorrect: 0,
     seconds: 60,
@@ -72,7 +73,7 @@ start: function() {
     for (var i = 0; i < questions.length; i++) {
         $(formDiv).append("<p>" + questions[i].question + "</p>");
         for (var t = 0; t < questions[i].options.length; t++) {
-            $(formDiv).append(" <label class='radio-inline'><input type='radio' name='question-" + i + "' value='" + questions[i].options[t] + "'> " + questions[i].options[t] + "</label>");
+            $(formDiv).append(" <label class='radio-inline'><input type='radio' name='question-" + i + "' id='" + questions[i].options[t] + "'> " + questions[i].options[t] + "</label>");
         }
     }
     $(formDiv).append("<br><br><button type='button' class='btn btn-dark' id='finishQuiz'>Submit</button>");
@@ -86,15 +87,12 @@ start: function() {
 done: function() {
 
     for (var i = 0; i < questions.length; i++) {
-        $("input[name='question[i]']:checked"), function() {
-            if ($(input[i]).val() == questions[i].answer) {
+            if ($("input[name=question-" + i + "]:checked").attr("id") == questions[i].answer) {
                 quiz.correct++;
             } else {
                 quiz.incorrect++;
             }
-        }
     }
-
     this.results();
 
 },
